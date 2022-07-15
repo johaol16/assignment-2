1aa6b73d1016fb296dd2e05c9ad703f9c33fdb00
Johanna Olsson <johanna.olsson@se.experis.com>
2022-07-15 14:31:40 +0200

    add translation history

---
+++ src/components/Profile/ProfileHeader.jsx
@@ -0,0 +1,9 @@
+const ProfileHeader = ({ username }) => {
+    return (
+        <header>
+            <h4>Hello { username }, welcome back!</h4>
+        </header>
+    )
+}
+
+export default ProfileHeader
