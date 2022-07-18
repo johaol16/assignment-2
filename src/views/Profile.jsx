d91ec056e9f8c54c6ddd49c8844b3a0372469667
Johanna Olsson <johanna.olsson@se.experis.com>
2022-07-18 12:00:45 +0200

    saved translations in storage

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -3,11 +3,27 @@ import ProfileHeader from "../components/Profile/ProfileHeader"
 import ProfileTranslationHistory from "../components/Profile/ProfileTranslationHistory"
 import withAuth from "../hoc/withAuth"
 import { useUser } from "../context/UserContext"
+import { useEffect } from "react"
+import { userById } from "../api/user"
+import { storageSave } from "../utils/storage"
+import { STORAGE_KEY_USER } from "../const/storageKeys"
 
 
 const Profile = () => {
 
-    const { user } = useUser()
+    const { user, setUser } = useUser()
+
+    useEffect(() => {
+        const findUser = async () => {
+            const [error, latestUser] = await userById(user.id)
+            if (error===null) {
+                storageSave(STORAGE_KEY_USER, latestUser)
+                setUser(latestUser)
+            }
+        }
+
+        //findUser()
+    }, [setUser, user.id])
 
     return (
         <>
--- src/views/Translation.jsx
+++ src/views/Translation.jsx
@@ -2,16 +2,28 @@ import withAuth from "../hoc/withAuth"
 import TranslationBox from "../components/Translation/TranslationBox"
 import InputBox from "../components/Translation/InputBox"
 import { useUser } from "../context/UserContext"
+import { translationAdd } from "../api/translation"
+import { storageSave } from "../utils/storage"
+import { STORAGE_KEY_USER } from "../const/storageKeys"
 
 
 const Translation = () => {
 
-    const { user } = useUser()
+    const { user, setUser } = useUser()
 
-    const handleTranslationClick = notes => {
-        console.log(notes.trim())
+    const handleTranslationClick = async (notes) => {
+        const translation = notes.trim()
 
-    
+        const [error, updatedUser] = await translationAdd(user, translation) 
+        if(error!==null) {
+            return
+        }
+
+        storageSave(STORAGE_KEY_USER, updatedUser)
+        setUser(updatedUser)
+
+        console.log("Error", error)
+        console.log("Result", updatedUser)
         
     }
 
